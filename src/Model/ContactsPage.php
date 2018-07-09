d21833abe7ac331d2dd2311f6d7b78bdae61fd6d
greeflas <vldmr.kuprienko@gmail.com>
2018-07-09 21:03:17 +0300

    Adds PHPDoc blocks

--- src/Model/ContactsPage.php
+++ src/Model/ContactsPage.php
@@ -2,22 +2,51 @@
 
 namespace App\Model;
 
+/**
+ * Model for contacts page.
+ *
+ * @author Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ */
 class ContactsPage
 {
+    /**
+     * @var string
+     */
     private $email;
+    /**
+     * @var string
+     */
     private $quote;
 
+    /**
+     * Constructor.
+     *
+     * @param string $email
+     * @param string $quote
+     *
+     * @throws \RuntimeException If email is empty.
+     */
     public function __construct(string $email, string $quote)
     {
+        if (empty($email)) {
+            throw new \RuntimeException('Email cannot be empty');
+        }
+
         $this->email = $email;
         $this->quote = $quote;
     }
 
+    /**
+     * @return string
+     */
     public function getEmail(): string
     {
         return $this->email;
     }
 
+    /**
+     * @return string
+     */
     public function getQuote(): string
     {
         return $this->quote;
--- src/Service/MessageGenerator.php
+++ src/Service/MessageGenerator.php
@@ -4,8 +4,16 @@ namespace App\Service;
 
 use Faker\Factory;
 
+/**
+ * Service for generating fake messages for testing.
+ *
+ * @author Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ */
 class MessageGenerator
 {
+    /**
+     * @return string Fake string.
+     */
     public function generate(): string
     {
         $faker = Factory::create();
