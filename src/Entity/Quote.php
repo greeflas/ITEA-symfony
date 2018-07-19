73c5f22f19ed01ae6d57dd3582099e135d7e4318
greeflas <vldmr.kuprienko@gmail.com>
2018-07-19 19:56:25 +0300

    Creates Quote entity
    - ./bin/console make:entity

---
+++ src/Entity/Quote.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\QuoteRepository")
+ */
+class Quote
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=155)
+     */
+    private $message;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
